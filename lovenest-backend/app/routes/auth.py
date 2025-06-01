2cd758ec75500091ad4de228c1ba952b9a67d64e
Chay <chaitanyakumarbandi@gmail.com>
2025-06-01 03:34:28 -0500

    Implemented Login and Authentication using JWT

--- lovenest-backend/app/routes/auth.py
+++ lovenest-backend/app/routes/auth.py
@@ -1,9 +1,8 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel, EmailStr
 from passlib.context import CryptContext
 from app.services.db import users_collection
-
-from fastapi import Depends
+from app.utils.jwt_handler import create_access_token
 
 
 #Initialize the router
@@ -12,7 +11,7 @@ router = APIRouter()
 #Setup for password hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-#Request Body Schema
+#Request Body Schema for signup
 class SignupRequest(BaseModel):
     full_name: str
     username: str
@@ -54,3 +53,38 @@ def signup(user: SignupRequest):
     except Exception as e:
         print("ERROR in signup:", str(e))
         raise HTTPException(status_code=500, detail="Something went wrong.")
+
+#--------------------LOGIN API -----------------------------------#
+
+#Request body for login
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+@router.post("/login")
+def login(user: LoginRequest):
+    try:
+        # Step 1: Find the user
+        existing_user = users_collection.find_one({"username": user.username})
+        if not existing_user:
+            raise HTTPException(status_code=401, detail="Invalid username or password")
+        
+        # Step 2: Check password
+        is_valid = pwd_context.verify(user.password, existing_user["password"])
+        if not is_valid:
+            raise HTTPException(status=401, detail="Invalid username or password")
+        
+        # Step 3: Generate JWT token
+        token = create_access_token(data={"username": user.username})
+
+        # Step 4: Return the token
+        return {
+            "access_token": token,
+            "token_type": "bearer"
+        }
+    except HTTPException as he:
+        return he
+    
+    except Exception as e:
+        print("Login Error: str(e)")
+        return HTTPException(status_code=500, detail="Something went wrong.")
--- lovenest-backend/app/utils/jwt_handler.py
+++ lovenest-backend/app/utils/jwt_handler.py
@@ -1,5 +1,21 @@
 from jose import jwt
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 from decouple import config
 
 #Secret key for signing the token
+JWT_SECRET = config("JWT_SECRET", default="your-fallback-secret-key") #default="your-temp-fallback-secret-key" if it can't find or not defined the actual JWT_SECRET in .env
+JWT_ALGORITHM = "HS256"
+
+def create_access_token(data: dict, expires_in: int=30):
+    """
+    Generates a JWT token.
+    :param data: Dictionary to encode (e.g., {"username": "cbandi"})
+    :param expires_in: Token expiry in minutes
+    """
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=expires_in)
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    return encoded_jwt
+    
